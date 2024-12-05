2a75888289df7d6d583a97af0b257f213d08a3c2
Prakash Singh <168165039+rawatcode652@users.noreply.github.com>
2024-12-05 07:41:00 +0000

    console config true

---
+++ 01_basics/datatype.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all code as newer version
+
+// alert(3+3)// we are using node js not browser
+console.log(3
+    +
+    3)  //Code readability should be high
+console.log("Prakash")
+
+let name = "Preesha"
+let age = 35
+let isloddedin = false
+
+//number => 2  to power 53
+//bigint
+//string => "hi"
+//boolean => false/true
+//null
---
+++ 01_basics/variable.js
@@ -0,0 +1,19 @@
+const accountId = 14445
+let acoountEmail = "rawatprakash@gmail.com"
+var accountPassword= "1234"
+accountCity="dehradun"
+//accountId = 2  not allowed
+accountEmail ="admin@gmail.com"
+accountPassword="3434"
+accountCity="Delhi"
+let accountState;
+//
+//console.log(accountId);
+
+/*
+Prefer not to use var bacause if issue in block
+scope and funtion scope
+
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+       
